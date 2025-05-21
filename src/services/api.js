152d9f0b1bbfb8f93ccb3b1395633c9f0f9094bb
Tiago Agostinho <tiagoagostinho49@gmail.com>
2025-05-21 18:04:45 -0300

    mudança da minha baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://burger-project-snfr.vercel.app'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
